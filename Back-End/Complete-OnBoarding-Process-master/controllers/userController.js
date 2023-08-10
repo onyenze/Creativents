cc4e1ced5c2de9958b813bf115c7f77279c850b0
onyenze <chibuezeonyenze123@gmail.com>
2023-08-10 11:40:00 +0100

    links

--- Back-End/Complete-OnBoarding-Process-master/controllers/userController.js
+++ Back-End/Complete-OnBoarding-Process-master/controllers/userController.js
@@ -111,7 +111,7 @@ const resendEmailVerification = async(req, res)=>{
             })
         }else {
             const verified = await userModel.findByIdAndUpdate(user._id, {isVerified: true}); //This should not be here.
-            const token = await jwt.sign({email}, process.env.JWT_SECRET, {expiresIn: "5m"});
+            const token = await jwt.sign({user}, process.env.JWT_SECRET, {expiresIn: "5m"});
             await jwt.verify(token, process.env.JWT_SECRET, (err)=>{
                 if(err) {
                     res.json('This Link is Expired. Please try again')
@@ -122,7 +122,7 @@ const resendEmailVerification = async(req, res)=>{
                         })
                     } else {
                         const subject = 'Kindly RE-VERIFY'
-                        const link = `https://creativents.onrender.com/api/verify/${user._id}/${token}`
+                        const link = `https://creativents.onrender.com/api/verify/${token}`
                         const message = `Welcome onBoard, kindly use this link ${link} to re-verify your account. Kindly note that this link will expire after 5(five) Minutes.`
                         sendEmail({
                             email: user.email,
@@ -262,7 +262,7 @@ const forgotPassword = async (req, res)=>{
                 id:isEmail.id
             }, process.env.JWT_SECRET, {expiresIn: '5m'})
             const subject = 'Link for Reset password'
-            const link = `https://creativents.onrender.com/api/changepassword?id=?token=${isEmail._id}/${token}`
+            const link = `https://creativents.onrender.com/api/changepassword/${isEmail._id}/${token}`
             const message = `Forgot your Password? it's okay, kindly use this link ${link} to re-set your account password. Kindly note that this link will expire after 5(five) Minutes.`
             sendEmail({
                 email,
