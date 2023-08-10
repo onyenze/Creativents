a98170e9f4ddf20e8edd30445c0c1b17ee527069
Karimtaiye <olayinkahassan117@gmail.com>
2023-08-10 23:12:17 +0100

    persist

--- Front-End/vite-project/src/Components/Redux/Store.jsx
+++ Front-End/vite-project/src/Components/Redux/Store.jsx
@@ -1,9 +1,38 @@
+
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query'
+import storage from 'redux-persist/lib/storage'
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
 import { eventReducers } from "./State";
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, eventReducers);
+
+
 export const store = configureStore({
-    // name:"Creativent",
-    reducer : {
-        events:eventReducers.reducer
-    }
+    reducer: {eventReducers: persistedReducer},
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+    // reducer : {
+    //     events:eventReducers.reducer
+    // }
 })
+setupListeners(store.dispatch)
+
+
--- Front-End/vite-project/src/main.jsx
+++ Front-End/vite-project/src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './Components/Redux/Store.jsx'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist'
+
+let persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 )
-  
