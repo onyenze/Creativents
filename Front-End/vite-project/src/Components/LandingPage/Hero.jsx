e56aba1e1be21b7d154490c4a4c6b9bb896732e0
Karimtaiye <olayinkahassan117@gmail.com>
2023-08-14 17:00:32 +0100

    new update

--- Front-End/vite-project/src/Components/LandingPage/Hero.jsx
+++ Front-End/vite-project/src/Components/LandingPage/Hero.jsx
@@ -1,73 +1,54 @@
-// import{AiOutlineSearch, AiOutlineArrowRight} from 'react-icons/ai'
+import React, { useState, useEffect } from 'react';
+import { AiOutlineSearch, AiOutlineArrowRight } from 'react-icons/ai';
 
-// function Hero (){
-//     return(
-//         <div>
-//             <div className="hero-section">
-//             <div className="event-result">
+function Hero() {
+    const imageChange = [
+        "./src/image/partyimage-one.webp",
+        "./src/image/event2.jpg",
+        "./src/image/event3.jpg",
+        // "./src/image/event4.jpg"
+    ];
+    const [imageIndex, setImageIndex] = useState(0);
 
-//                 <div className="discription">
-//                     <h1>DON'T MISS THE UPCOMING EVENT</h1>
-//                     <h2>Explore what's happening, where and when</h2>
-//                 </div>
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImageIndex((prevIndex) => (prevIndex + 1) % imageChange.length);
+        }, 5000); // Adjust the interval time for a slower slide effect (e.g., 5000ms for 5 seconds)
 
-//                 <div className="search-event">
-//                     <button className='search-bar'>
-//                     <input type="text" placeholder="Search Event" name="search"></input>
-//                         <AiOutlineSearch className='searchin'/>
-                        
-//                     </button>
+        return () => clearInterval(interval);
+    }, []);
 
-//                     <button className='see-result'>See result
-//                     <AiOutlineArrowRight className='arrow'/>
-//                     </button>
-
-//                 </div>
-//             </div>
-            
-          
-//         </div>
-//         </div>
-//     )
-// }
-
-// export default Hero
-
-import{AiOutlineSearch, AiOutlineArrowRight} from 'react-icons/ai'
-function Hero (){
-
-    return(
+    return (
         <div>
             <div className="hero-section">
-
-            {/* <img src={imageChange[image % imageChange.length]} alt="" /> */}
-            <div className="event-result">
-
-                <div className="discription">
-                    <h1>DON'T MISS THE UPCOMING EVENT</h1>
-                    <h2>Explore what's happening, where and when</h2>
-                </div>
-
-                <div className="search-event">
-                    <div className='search-bar'>
-                    <input type="text" placeholder="Search Event" name="search"></input>
-                        <AiOutlineSearch className='searchin'/>  
+                <img
+                    src={imageChange[imageIndex]}
+                    alt=""
+                    style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                />
+                <div className="event-result">
+                    <div className="discription">
+                        <h1>DON'T MISS THE UPCOMING EVENT</h1>
+                        <h2>Explore what's happening, where and when</h2>
                     </div>
-                    
-                    <div className='see-result2'>
-                    <button className='see-result'>See result
-                    <AiOutlineArrowRight className='arrow'/>
-                    </button>
-                    </div>
-                    
 
+                    <div className="search-event">
+                        <div className="search-bar">
+                            <input type="text" placeholder="Search Event" name="search" />
+                            <AiOutlineSearch className="searchin" />
+                        </div>
+
+                        <div className="see-result2">
+                            <button className="see-result">
+                                See result
+                                <AiOutlineArrowRight className="arrow" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
-          
         </div>
-        </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
