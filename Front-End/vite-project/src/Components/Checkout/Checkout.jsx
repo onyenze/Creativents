1a9aa2be951564902693d6fa6d76c8c8e4aaa47d
Karimtaiye <olayinkahassan117@gmail.com>
2023-08-15 12:39:06 +0100

    yeah

--- Front-End/vite-project/src/Components/Checkout/Checkout.jsx
+++ Front-End/vite-project/src/Components/Checkout/Checkout.jsx
@@ -43,85 +43,91 @@ const Checkout = () =>{
 
     return(
         <>
+       {
+        !data? <h1 style={{
+            fontSize:"26px", color:"white", width:"100%",
+            height:"100vh", textAlign:"center"
+        }}>Loading, Please wait...</h1>:
         <div className="checkoutcontainer">
 
-            <div className="checkoutholder">
+        <div className="checkoutholder">
 
-                <div className="checkoutlogo">
-                <div className="checkoutimage">
-                <img src={LogoC} alt=""/>
-                <h2>reactivent</h2>
-                </div>
-                </div>
-
-                <div className='checkouteventimage'>
-                    <div className='imagecheckout'>
-                        <img src={data.eventImages} alt="" />
-                    </div>
-                {/* <div className='commentsection'> */}
-                            {/* <div className='todolistcomment'>
-                                <div className='userprofile'></div>
-                                <input className='comment' type="text" />
-                                <button className='send'>Send</button>
-                            </div> */}
-                            {/* <div className='dropdown'></div> */}
-                        {/* </div> */}
-                </div>
-                    
-                    {/* <p>{data.eventName}</p> */}
-                    <p>The Curve Africa Final Project HackAthon Presentation</p>
-                    <div className="checkouteventdetails">
-                        <div className='checkoutvenue'>
-                            <div className='checkoutdetails'>
-                                <BsCalendarDate/>
-                                {/* <p>{data.eventDate}</p> */}
-                            </div>
-                            <div className='checkoutdetails'>
-                                <BiTimeFive/>
-                                {/* <p>{data.eventTime}</p> */}
-                            </div>
-                            <div className='checkoutdetails'>
-                                <CiLocationOn/>
-                                {/* <p>{data.eventVenue}</p> */}
-                            </div>
-                        </div>
-                        <div className='checkoutticket'>
-                            <h2>Ticket Details</h2>
-
-                            <div className='allticket'>
-                                <h2>Ticket Quantity</h2>
-                                <div className='chooseticket'>
-                                    <button className='buttonticket' onClick={incrementQuantity}>+</button>
-                                    <h5>{ticketQuantity}</h5>
-                                    <button className='buttonticket' onClick={decrementQuantity}>-</button>
-                                </div>
-                            </div>
-
-                            <div className='quantity'>
-                                    <h2>Price</h2>
-                                    <h3>{ticketPrice}</h3>
-                                </div>
-
-                                <div className='totalamount'>
-                                    <h2>Total</h2>
-                                    <h3>{totalAmount}</h3>
-                                </div>
-                        </div>
-                    </div>
-
-                    <div className="checkoutdescription-checkout">
-                        <div className="checkoutdescription">
-                            <h1>Description</h1>
-                            <p>The Hackathon Farewell Day is a fun and collaborative event where all members of the software program come together for a final hurrah. It's an opportunity to look back at the milestones we’ ve achieved, the challenges we overcame, and the memories we created during the program's lifetime.</p>
-                        </div>
-
-                       
-
-                        <button className='booknow'>Book now</button>
-
-                    </div>
+            <div className="checkoutlogo">
+            <div className="checkoutimage">
+            <img src={LogoC} alt=""/>
+            <h2>reactivent</h2>
             </div>
+            </div>
+
+            <div className='checkouteventimage'>
+                <div className='imagecheckout'>
+                    <img src={data.eventImages} alt="" />
+                </div>
+            {/* <div className='commentsection'> */}
+                        {/* <div className='todolistcomment'>
+                            <div className='userprofile'></div>
+                            <input className='comment' type="text" />
+                            <button className='send'>Send</button>
+                        </div> */}
+                        {/* <div className='dropdown'></div> */}
+                    {/* </div> */}
+            </div>
+                
+                {/* <p>{data.eventName}</p> */}
+                <p>The Curve Africa Final Project HackAthon Presentation</p>
+                <div className="checkouteventdetails">
+                    <div className='checkoutvenue'>
+                        <div className='checkoutdetails'>
+                            <BsCalendarDate/>
+                            {/* <p>{data.eventDate}</p> */}
+                        </div>
+                        <div className='checkoutdetails'>
+                            <BiTimeFive/>
+                            {/* <p>{data.eventTime}</p> */}
+                        </div>
+                        <div className='checkoutdetails'>
+                            <CiLocationOn/>
+                            {/* <p>{data.eventVenue}</p> */}
+                        </div>
+                    </div>
+                    <div className='checkoutticket'>
+                        <h2>Ticket Details</h2>
+
+                        <div className='allticket'>
+                            <h2>Ticket Quantity</h2>
+                            <div className='chooseticket'>
+                                <button className='buttonticket' onClick={incrementQuantity}>+</button>
+                                <h5>{ticketQuantity}</h5>
+                                <button className='buttonticket' onClick={decrementQuantity}>-</button>
+                            </div>
+                        </div>
+
+                        <div className='quantity'>
+                                <h2>Price</h2>
+                                <h3>{ticketPrice}</h3>
+                            </div>
+
+                            <div className='totalamount'>
+                                <h2>Total</h2>
+                                <h3>{totalAmount}</h3>
+                            </div>
+                    </div>
+                </div>
+
+                <div className="checkoutdescription-checkout">
+                    <div className="checkoutdescription">
+                        <h1>Description</h1>
+                        <p>The Hackathon Farewell Day is a fun and collaborative event where all members of the software program come together for a final hurrah. It's an opportunity to look back at the milestones we’ ve achieved, the challenges we overcame, and the memories we created during the program's lifetime.</p>
+                    </div>
+
+                   
+
+                    <button className='booknow'>Book now</button>
+
+                </div>
         </div>
+    </div>
+       }
         </>
     )
 }
