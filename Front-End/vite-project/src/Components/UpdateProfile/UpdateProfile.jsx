4ae8302653b7c0f777d2aa747d5f50f25da721dc
Karimtaiye <olayinkahassan117@gmail.com>
2023-08-20 21:11:50 +0100

    pushed

--- Front-End/vite-project/src/Components/UpdateProfile/UpdateProfile.jsx
+++ Front-End/vite-project/src/Components/UpdateProfile/UpdateProfile.jsx
@@ -3,14 +3,17 @@ import './UpdateProfile.css'
 // import './UpdateMedia.css'
 import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AiFillHome } from 'react-icons/ai'
+import { MdCreateNewFolder } from 'react-icons/md'
+import { BsFillCheckSquareFill } from 'react-icons/bs'
 import { userStoreData } from '../Redux/State'
-import {AiFillHome} from 'react-icons/ai'
-import {MdCreateNewFolder} from 'react-icons/md'
-import {BsFillCheckSquareFill} from 'react-icons/bs'
 // import { userProfile } from '../Redux/State'
 
 function UpdateProfile() {
+    const nav = useNavigate()
+    const [loading, setLoading] = useState(false)
+    const [visible, setVisible] = useState(false)
     const Dispatch = useDispatch()
     const userOnLoggedIn = useSelector(state=>state.events.user)
     const initUpdates = useSelector(state=>state.events.userInitUpdate)
@@ -18,6 +21,7 @@ function UpdateProfile() {
     const initLastName = initUpdates.lastname
     const initEmail = initUpdates.email
     const { id } = useParams()
+    const [updatesucc, setUpdatesucc] = useState("")
     const [profilePicture, setProfilePicture] = useState([])
     const [firstname, setFirstName] = useState(initFirstName)
     const [lastname, setLastname] = useState(initLastName)
@@ -36,6 +40,7 @@ function UpdateProfile() {
     
     const url = `https://creativents-on-boarding.onrender.com/api/add-profile-image/${id}`
     const addProfilePicture = () => {
+        setLoading(true)
         axios.put(url, formData, {
             headers : {
                 'Content-Type': 'multipart/form-data',
@@ -44,6 +49,10 @@ function UpdateProfile() {
         } )
         .then(res=>{
             console.log(res)
+            setVisible(true)
+            setLoading(false)
+            setUpdatesucc("Profile Update Successfully")
+            nav('/homepage')
             Dispatch(userStoreData({
                 email:res.data.data.email, 
                 id:res.data.data._id,
@@ -54,7 +63,18 @@ function UpdateProfile() {
             }))
         })
         .catch(err=>{
-            console.log(err);
+            console.log(err)
+            setLoading(false)
+            setVisible(true);
+            setTimeout(() => {
+            setVisible(false);
+              }, 3000)
+            if(err.message === "Network Error"){
+                setUpdatesucc("Please check your Internet Connection")
+            }
+            else{        
+                setUpdatesucc("Cannot Update Profile")
+              }
         })
         
     }
@@ -85,8 +105,8 @@ function UpdateProfile() {
                      <label>Profile Name</label>
                     <input type="text"   onChange={(e)=>setProfileName(e.target.value)}/>
                     <div className='Update_Buttons'>
-                    <button className='Cancel_Btn'>Cancel</button>
-                    <button className='Update_Btn' onClick={addProfilePicture}>Save</button>
+                    <button className='Cancel_Btn'onClick={()=>nav('/homepage')}>Cancel</button>
+                    <button style={{background:loading?"rgb(126, 87, 10)":null}} className='Update_Btn' onClick={addProfilePicture}>{loading?"Uploading":"Save"}</button>
                     </div>
                 </section>
             </div>
@@ -108,6 +128,12 @@ function UpdateProfile() {
             </div>
           </div>
         </section>
+
+       {
+        visible? <div className='ProfilePopUp'>
+            <h4>{updatesucc}</h4>
+        </div>:null
+       }
     </div>
   )
 }
