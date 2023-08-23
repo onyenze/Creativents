0769adddd5438ef808510236d32040689d4598bb
onyenze <chibuezeonyenze123@gmail.com>
2023-08-23 15:13:52 +0100

    changes

--- Back-End/Complete-OnBoarding-Process-master/controllers/adminController.js
+++ Back-End/Complete-OnBoarding-Process-master/controllers/adminController.js
@@ -141,32 +141,32 @@ const allLoginUsers = async (req, res)=>{
 
 
 // Admin dashboard to view reported items
-router.get('/admin/reports', async (req, res) => {
-  try {
-    const reports = await reportModel.find().populate('reporter targetId').exec();
-    res.render('admin-dashboard', { reports });
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching reports', error: error.message });
-  }
-});
+// router.get('/admin/reports', async (req, res) => {
+//   try {
+//     const reports = await reportModel.find().populate('reporter targetId').exec();
+//     res.render('admin-dashboard', { reports });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error fetching reports', error: error.message });
+//   }
+// });
 
 // Route to handle actions on reported items (e.g., delete, warn user, etc.)
-router.post('/admin/reports/:reportId', async (req, res) => {
-  try {
-    const reportId = req.params.reportId;
-    const action = req.body.action; // Example: 'delete', 'warn'
+// router.post('/admin/reports/:reportId', async (req, res) => {
+//   try {
+//     const reportId = req.params.reportId;
+//     const action = req.body.action; // Example: 'delete', 'warn'
     
-    // Handle the action based on your business logic
-    // For example, you can delete the reported item, warn the user, etc.
+//     // Handle the action based on your business logic
+//     // For example, you can delete the reported item, warn the user, etc.
     
-    // Update the report status (e.g., resolved, action taken)
-    const report = await reportModel.findByIdAndUpdate(reportId, { status: 'resolved', actionTaken: action });
+//     // Update the report status (e.g., resolved, action taken)
+//     const report = await reportModel.findByIdAndUpdate(reportId, { status: 'resolved', actionTaken: action });
     
-    res.json({ message: 'Action taken successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error taking action on report', error: error.message });
-  }
-});
+//     res.json({ message: 'Action taken successfully' });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error taking action on report', error: error.message });
+//   }
+// });
 
 
 
